ab59039d19c226086d01561d0ecf78c49a042bed
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-02-26 17:24:47 -0300

    Add menu DropDown

--- src/components/headers/menuDropDown/index.js
+++ src/components/headers/menuDropDown/index.js
@@ -8,6 +8,17 @@ export default prosp => {
             <div>
                 <span>Olá Gilson</span>
                 <h3>Contas e Listas</h3>
+                <ul>
+                    <li>
+                        <ul>
+                            <li>
+                                <div>
+                                    <h1>Olá</h1>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </C.Container>
     )
--- src/components/headers/menuDropDown/menuDrop.js
+++ src/components/headers/menuDropDown/menuDrop.js
@@ -6,7 +6,26 @@ export const Container = styled.div`
    div{
     margin-left: 20px;
    }
+   
+   div li{
+      display: inline-block;
+   }
 
+   div li ul{
+      position: absolute;
+      display: none;
+   }
+
+   div li ul li div{
+      border: 1px solid red;
+      width: 500px;
+      height: 450px;
+   }
+
+   div:hover ul{
+      display: block;
+      
+   }
 
 
 `
